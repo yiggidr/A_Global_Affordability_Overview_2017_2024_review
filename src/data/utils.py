8498a98f6ac7cb059415a5ae843af11cf3e3e8ab
aelbaz <avner3500@gmail.com>
2026-02-22 16:16:51 +0000

    Minor modifs

--- src/data/utils.py
+++ src/data/utils.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 
 def configure_s3(con: duckdb.DuckDBPyRelation):
     load_dotenv()
-    s3_endpoint = os.getenv("AWS_S3_ENDPOINT", "minio.lab.sspcloud.fr").replace(
-        "https://", ""
-    )
+    # s3_endpoint = os.getenv("AWS_S3_ENDPOINT", "minio.lab.sspcloud.fr").replace(
+    #     "https://", ""
+    # )
 
-    con.execute("INSTALL httpfs; LOAD httpfs;")
-    con.execute(f"SET s3_endpoint='{s3_endpoint}';")
+    # con.execute("INSTALL httpfs; LOAD httpfs;")
+    # con.execute(f"SET s3_endpoint='{s3_endpoint}';")
     con.execute(f"SET s3_access_key_id='{os.getenv('AWS_ACCESS_KEY_ID')}';")
     con.execute(f"SET s3_secret_access_key='{os.getenv('AWS_SECRET_ACCESS_KEY')}';")
     con.execute("SET s3_url_style='path';")
